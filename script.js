78ae2503e9d29e684778b18c1b34d3fd429f6031
NandoAbdillah <nandoabdilah1606@gmail.com>
2023-09-23 09:56:50 +0700

    versi 1.2
    JS QuerySelector

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
-let i =0;
-function tampil(konten) {
-    i++;
-    konten.innerHTML ='JavaScript';
-    document.querySelector('#aku').append(` diklik sebanyak ${i}x`); // sebenarnya bisa pake konten
+// let i =0;
+// function tampil(konten) {
+//     i++;
+//     konten.innerHTML ='JavaScript';
+//     document.querySelector('#aku').append(` diklik sebanyak ${i}x`); // sebenarnya bisa pake konten
+// }
+
+function tampil() {
+    //QuerySelector
+    document.querySelector('h1').textContent='Belajar JavaScript';
+    document.querySelector('.container').style.backgroundColor="red";
+    document.querySelector('.container').style.color="white";
+    document.querySelector('#p1').textContent='Belajar JavaScript untuk membuat web kita menjadi lebih interaktif';
+    document.querySelector('#p1').style.fontStyle='italic';
 }
