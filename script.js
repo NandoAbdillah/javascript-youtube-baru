4e70dee1274f7a0337e40b6ae592990ef609a081
NandoAbdillah <nandoabdilah1606@gmail.com>
2023-09-23 10:46:46 +0700

    versi 1.4
    JS Form

--- script.js
+++ script.js
@@ -5,16 +5,24 @@
 //     document.querySelector('#aku').append(` diklik sebanyak ${i}x`); // sebenarnya bisa pake konten
 // }
 
+// function tampil() {
+//     //memeriksa array img
+//     // console.log(document.querySelectorAll('img')); //fungsi querySelctorAll adalah untuk menunjuk semua elemen img yang sama dimana nanti akan membentuk sebuah array sesuai urutan elemen img tersebut di html
+
+//     //membuat random untuk index
+//     min = Math.ceil(0);
+//     max = Math.floor(2);
+
+//     let n = Math.floor(Math.random()* (max - min +1));
+//     console.log(n);
+//     document.querySelectorAll('img')[n].style.visibility='visible';
+
+// }
+
 function tampil() {
-    //memeriksa array img
-    // console.log(document.querySelectorAll('img')); //fungsi querySelctorAll adalah untuk menunjuk semua elemen img yang sama dimana nanti akan membentuk sebuah array sesuai urutan elemen img tersebut di html
-
-    //membuat random untuk index
-    min = Math.ceil(0);
-    max = Math.floor(2);
-
-    let n = Math.floor(Math.random()* (max - min +1));
-    console.log(n);
-    document.querySelectorAll('img')[n].style.visibility='visible';
-
+    alert( 'Nama Anda : ' +
+    document.querySelector('#inputNama').value
+    )
+    document.querySelector('#tempelNama').value= document.querySelector('#inputNama').value;
+    document.querySelector('#para').textContent= document.querySelector('#inputNama').value;
 }
